b555dcbfe36360c37227911ad09240ef3fd8e22d
ChristianDPeralta <christianperalta656@gmail.com>
2025-05-17 17:09:03 +0800

    Update PostLIst.jsx

--- src/components/PostLIst.jsx
+++ src/components/PostLIst.jsx
@@ -4,7 +4,12 @@ function PostList({ posts, onEdit, onDelete }) {
   return (
     <div className="post-list">
       {posts.map((post) => (
-        <PostCard key={post.id} post={post} onEdit={onEdit} onDelete={onDelete} />
+        <PostCard
+          key={post.id}
+          post={post}
+          onEdit={onEdit}
+          onDelete={onDelete}
+        />
       ))}
     </div>
   );
