a93cf7809f82534f06ced9bf1fdcd4b44af81108
ChristianDPeralta <christianperalta656@gmail.com>
2025-05-17 17:01:20 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API_BASE = "https://final-api-vr8u.onrender.com/api/posts";
+const USER_BASE = "https://final-api-vr8u.onrender.com/api/users";
 
+// --- Post-related APIs ---
 export const getPosts = () => axios.get(API_BASE);
 export const createPost = (data) => axios.post(API_BASE, data);
 export const updatePost = (id, data) => axios.put(`${API_BASE}/${id}`, data);
@@ -11,8 +13,20 @@ export const deletePost = (id) => axios.delete(`${API_BASE}/${id}`);
 export const getComments = (postId) =>
   axios.get(`${API_BASE}/${postId}/comments`);
 
-export const addComment = (postId, commentData) =>
-  axios.post(`${API_BASE}/${postId}/comments`, commentData);
+// Make sure to provide userId as a param
+export const addComment = (postId, commentData, userId) =>
+  axios.post(
+    `${API_BASE}/${postId}/comments`,
+    commentData,
+    { params: { userId } }
+  );
 
 export const deleteComment = (postId, commentId) => 
   axios.delete(`${API_BASE}/${postId}/comments/${commentId}`);
+
+// --- User/Profile-related APIs ---
+export const getUserProfile = (userId) =>
+  axios.get(`${USER_BASE}/${userId}`);
+
+export const updateUserProfile = (userId, profileData) =>
+  axios.put(`${USER_BASE}/${userId}/profile`, profileData);
