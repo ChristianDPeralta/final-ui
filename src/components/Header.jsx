07a3d26fc780954790a01662adeecfdf1f1fdbbd
ChristianDPeralta <christianperalta656@gmail.com>
2025-05-17 17:10:05 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
+import { useUser } from "../UserContext";
+
 function Header() {
+  const user = useUser();
   return (
-    <header className="header">
-      <h1>
-        <span className="highlight">Social</span>Sphere
-      </h1>
+    <header style={{
+      background: "#1976d2",
+      color: "#fff",
+      padding: "18px 0",
+      textAlign: "center",
+      marginBottom: 24,
+      fontSize: "1.4em",
+      fontWeight: 700,
+    }}>
+      SocialSphere
+      <span style={{
+        float: "right",
+        marginRight: 24,
+        fontSize: "1em",
+        fontWeight: 400,
+      }}>
+        {user?.displayName ? `👤 ${user.displayName}` : ""}
+      </span>
     </header>
   );
 }
