d2a9252d69fb7d2a0ae084f53fe8af399ad17c31
ChristianDPeralta <christianperalta656@gmail.com>
2025-05-17 15:57:16 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -6,3 +6,13 @@ export const getPosts = () => axios.get(API_BASE);
 export const createPost = (data) => axios.post(API_BASE, data);
 export const updatePost = (id, data) => axios.put(`${API_BASE}/${id}`, data);
 export const deletePost = (id) => axios.delete(`${API_BASE}/${id}`);
+
+// --- Comment-related APIs ---
+export const getComments = (postId) =>
+  axios.get(`${API_BASE}/${postId}/comments`);
+
+export const addComment = (postId, commentData) =>
+  axios.post(`${API_BASE}/${postId}/comments`, commentData);
+
+export const deleteComment = (postId, commentId) => 
+  axios.delete(`${API_BASE}/${postId}/comments/${commentId}`);
